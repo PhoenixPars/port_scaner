2dc56b9973ec2b8e2ad9c33132f44ba9b7d25fc8
Ashrarhack <60186819+ashrarhacka409@users.noreply.github.com>
2021-12-05 22:24:39 +0330

    Update exceptions.py

--- port_scaner/exceptions.py
+++ port_scaner/exceptions.py
@@ -1,9 +1,37 @@
 """port_scaner.exceptions"""
 
+########################################
+#       tool name : port scaner        #
+#  ----------------------------------  #
+#                                      #
+#   create by : #a409                  #
+#   my chanell: t.me/ashrarhack_apps   #
+#                                      #
+#                       version : 1.09 #
+########################################
 
 class ScannerNotFound(Exception):
     pass
 
+########################################
+#       tool name : port scaner        #
+#  ----------------------------------  #
+#                                      #
+#   create by : #a409                  #
+#   my chanell: t.me/ashrarhack_apps   #
+#                                      #
+#                       version : 1.09 #
+########################################
 
 class ScannerRequestError(Exception):
     pass
+
+########################################
+#       tool name : port scaner        #
+#  ----------------------------------  #
+#                                      #
+#   create by : #a409                  #
+#   my chanell: t.me/ashrarhack_apps   #
+#                                      #
+#                       version : 1.09 #
+########################################
