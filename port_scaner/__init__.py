675a614a223f302d402995481a6fdd71e33853b5
Ashrarhack <60186819+ashrarhacka409@users.noreply.github.com>
2021-12-05 22:24:01 +0330

    Create __init__.py

---
+++ port_scaner/__init__.py
@@ -0,0 +1,23 @@
+"""port_scaner"""
+
+########################################
+#       tool name : port scaner        #
+#  ----------------------------------  #
+#                                      #
+#   create by : #a409                  #
+#   my chanell: t.me/ashrarhack_apps   #
+#                                      #
+#                       version : 1.09 #
+########################################
+
+from port_scaner.core import Port_scaner
+
+########################################
+#       tool name : port scaner        #
+#  ----------------------------------  #
+#                                      #
+#   create by : #a409                  #
+#   my chanell: t.me/ashrarhack_apps   #
+#                                      #
+#                       version : 1.09 #
+########################################
